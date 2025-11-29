9416f7a3720ff10110d0261b8277798c54f1e175
sokhouw <marcin.sokolowski@gmail.com>
2025-11-29 09:07:04 +0100

    self-contained tmux.conf

--- .dotfiles/scripts/init-tmux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.local/share/tmux/plugins
-git clone https://github.com/tmux-plugins/tpm $HOME/.local/share/tmux/plugins/tpm
-git clone https://github.com/fabioluciano/tmux-tokyo-night $HOME/.local/share/tmux/plugins/tmux-tokyo-night
-git clone https://github.com/sainnhe/tmux-fzf $HOME/.local/share/tmux/plugins/tmux-fzf
