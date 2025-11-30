090b39bb01959cd069ea1bd078856568e30a9c19
sokhouw <marcin.sokolowski@gmail.com>
2025-11-30 09:12:05 +0100

    .dotfiles-backup moved .dotfiles/backup

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,14 +1,9 @@
-cat >> $HOME/.bashrc <<- "EOF"
-alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME" # sokhouw/dotfiles
-alias dfg=dotfiles                                               # sokhouw/dotfiles
-alias dfg-tree="dfg ls-files | tree --fromfile ."                # sokhouw/dotfiles
-alias dfg-other="dfg ls-files --other"                           # sokhouw/dotfiles
-EOF
+echo 'alias dfg="git --git-dir=$HOME/.dotfiles/repo --work-tree=$HOME" # sokhouw/dotfiles' >> $HOME/.bashrc
 source $HOME/.bashrc
 if ! dfg checkout; then
     for f in $(dfg checkout 2>&1 | grep -E "^\s" | sed "s/\s\+//"); do 
-        [ ! -d "$HOME/.dotfiles-backup/$(dirname $f)" ] && mkdir -p "$HOME/.dotfiles-backup/$(dirname $f)" 
-        mv $HOME/$f $HOME/.dotfiles-backup/$f
+        [ ! -d "$HOME/.dotfiles/backup/$(dirname $f)" ] && mkdir -p "$HOME/.dotfiles/backup/$(dirname $f)" 
+        mv $HOME/$f $HOME/.dotfiles/backup/$f
     done
     dfg checkout
 fi
