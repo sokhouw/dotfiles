8c9a7ac53d016b982a0b85ea31a6e85c22e29693
sokhouw <marcin.sokolowski@gmail.com>
2025-12-02 18:04:00 +0100

    manual elp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ require("config/clipboard")
 require("config/diagnostic")
 require("config/filetypes")
 require("config/options")
+require("config/lsp")
 require("config/lazy")
 
 require("themery").setThemeByName("vague", true)
