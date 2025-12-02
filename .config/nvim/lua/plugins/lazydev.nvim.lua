99ccad378245e3f0dd157a14e55f0fe39385fa40
sokhouw <marcin.sokolowski@gmail.com>
2025-12-02 17:16:32 +0100

    added lazydev

--- .config/nvim/lua/plugins/lazydev.nvim.lua
+++ .config/nvim/lua/plugins/lazydev.nvim.lua
@@ -10,20 +10,4 @@ return {
       },
     },
   },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-    },
-  },
 }
