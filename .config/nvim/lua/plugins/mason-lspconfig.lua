c011cd57e714df1c67e2e0a9cfd29d4c4b91a4d6
sokhouw <marcin.sokolowski@gmail.com>
2025-12-17 22:44:44 +0100

    fixed LSP config

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,5 +1,6 @@
 return {
   "mason-org/mason-lspconfig.nvim",
+  opts = {}, -- Important !!! Without this, lua-language-server never starts
   dependencies = {
     "mason-org/mason.nvim",
     "neovim/nvim-lspconfig",
