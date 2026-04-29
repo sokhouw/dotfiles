892c807077afbd801764ae0af017570015e4086a
sokhouw <marcin.sokolowski@gmail.com>
2026-04-29 08:02:15 +0200

    work in progress

--- .config/nvim/lsp/erlangls.lua
+++ .config/nvim/lsp/erlangls.lua
@@ -1,6 +1,6 @@
--- ---@type vim.lsp.Config
--- return {
---   cmd = "erlang_ls",
---   filetypes = { "erlang" },
---   root_markets = { "rebar.config", "rebar.config.script", ".git" },
--- }
+---@type vim.lsp.Config
+return {
+  cmd = { "erlang_ls" },
+  filetypes = { "erlang" },
+  root_markers = { "rebar.config", "rebar.config.script", ".git" },
+}
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,5 +1,5 @@
 -- Uncomment LSPs that are to be installed manually outside mason
--- vim.lsp.enable({"erlangls"})
 -- ELP (Erlang Language Platform) is manually enabled here as it's not configured via mason-lspconfig.
--- Ensure the 'elp' binary is available in your PATH.
-vim.lsp.enable({"elp"})
+-- Ensure the 'erlang_ls' binary is available in your PATH.
+vim.lsp.enable({"erlangls"})
+--vim.lsp.enable({"elp"})
