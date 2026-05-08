818f70d6a816cc9a4ef82f966a031aa4db218371
sokhouw <marcin.sokolowski@gmail.com>
2026-05-08 22:35:46 +0200

    sorted themes in Themery

--- .config/nvim/lua/plugins/themery.nvim.lua
+++ .config/nvim/lua/plugins/themery.nvim.lua
@@ -1,20 +1,23 @@
+local themes = {
+  "carbonfox", "duskfox", "nightfox", "terafox",
+  "catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato", "catppuccin-mocha",
+  "tokyonight-night", "tokyonight-moon", "tokyonight-storm", "tokyonight-day",
+  "vague",
+  "happy_hacking",
+  "nefertiti",
+  "quantum",
+  "spacegray",
+  "tender",
+  "vimbrains",
+  "vorange",
+}
+table.sort(themes)
+
 return {
   "zaldih/themery.nvim",
   lazy = false,
   opts = {
-    themes = {
-      "carbonfox", "duskfox", "nightfox", "terafox",
-      "catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato", "catppuccin-mocha",
-      "tokyonight-night", "tokyonight-moon", "tokyonight-storm", "tokyonight-day",
-      "vague",
-      "happy_hacking",
-      "nefertiti",
-      "quantum",
-      "spacegray",
-      "tender",
-      "vimbrains",
-      "vorange",
-    },
-    livePreview = true
-  }
+    themes = themes,
+    livePreview = true,
+  },
 }
